cc4bcb582a01eaec2e7aadd9ccae41544ce7d27a
AlexandreBessard <alexandre.bessard@gmail.com>
2024-03-09 16:25:43 +0100

    Chat & Message

---
+++ src/main/java/org/example/Message.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Message {
+    @JsonProperty("role")
+    private String role;
+
+    @JsonProperty("content")
+    private String content;
+
+    public Message(String role, String content) {
+        this.role = role;
+        this.content = content;
+    }
+
+}
